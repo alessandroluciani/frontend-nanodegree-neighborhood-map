05e51d0229ebbdf0274c44569f637df14042ce18
alessandroluciani <alessandroluciani@users.noreply.github.com>
2015-11-21 21:36:20 +0100

    css, markers and observables

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,34 +1,107 @@
+//Model Data
 var initialPlaces = [
 	{
-		name: 'Place 1',
-		lat: 'img/cat1.jpg',
-		lng: 'ALT CAT1'
+		name: 'Colosseum',
+		city: 'Rome',
+		state: 'Italy',
+		lat: 41.890210,
+		lng: 12.492231,
+		marker: null
 	},
 	{
-		name: 'Stadio Olimpico di Roma',
-		lat: 'img/cat1.jpg',
-		lng: 'ALT CAT1'
+		name: 'Olimpic Stadium',
+		city: 'Rome',
+		state: 'Italy',
+		lat: 41.933907,
+		lng: 12.454977,
+		marker: null
+	},
+	{
+		name: 'Venezia Square',
+		city: 'Rome',
+		state: 'Italy',
+		lat: 41.895268,
+		lng: 12.481222,
+		marker: null
+	},
+	{
+		name: 'Popolo Square',
+		city: 'Rome',
+		state: 'Italy',
+		lat: 41.910646,
+		lng: 12.476030,
+		marker: null
+	},
+	{
+		name: 'Termini Station',
+		city: 'Rome',
+		state: 'Italy',
+		lat: 41.900821,
+		lng: 12.501755,
+		marker: null
 	}
 ];
+
+
+var Place = function(data) {
+
+	this.name = ko.observable(data.name);
+	this.city = ko.observable(data.city);
+	this.state = ko.observable(data.state);
+	this.lat = ko.observable(data.lat);
+	this.lng = ko.observable(data.lng);
+	this.marker = ko.observable(data.marker);
+	
+
+	//computed observable
+	/*this.title = ko.computed(
+		function() {
+			var title;
+			var clicks = this.clickCount();
+			if(clicks <= 10){title = "pittolo pittolo";}
+			else if(clicks <= 20){title = "teen";}
+			else if(clicks <= 30){title = "adult";}
+			else {title = "bollito";}
+			return title;
+	},this);*/
+
+}
 
 
 var ViewModel = function() {
 
 	var self = this;
 	this.placesList = ko.observableArray([]);
-/*
-	initialPlaces.forEach(function(placeItem) {
-		console.log(placeItem);
-		//self.placesList.push( new Cat(placeItem) );
-	});*/
 
-	GoogleMap.init();
+	GoogleMap.init(function(){
+		initialPlaces.forEach(function(placeItem) {
+			self.placesList.push( new Place(placeItem) );
+		});
+	});
 
+
+
+
+	//da Fare
+	this.setPlace = function(catClicked) {
+		self.currentPlace(catClicked);
+	};
+
+	//da Fare
+	this.selectPlace = function(place) {
+		//this.clickCount(this.clickCount()+1);
+		//self.currentCat().clickCount(self.currentCat().clickCount()+1); //da usare solamente con self
+		console.log(place);
+	
+	};
 
 }
 
 
+
+
 //googleMap section
+var map;
 var GoogleMap = {
 
 	init: function(){
@@ -53,16 +126,30 @@ var GoogleMap = {
 			mapTypeId: google.maps.MapTypeId.HYBRID,
 			zoom: 13
 		};
-		this.render(mapOptions);
 
+		this.render(mapOptions);
 	},
 
 	render: function(mapOptions) {
-		var map = new google.maps.Map(document.getElementById('gmap'),mapOptions);
-	}
-	
-};
+		map = new google.maps.Map(document.getElementById('gmap'),mapOptions);
+		this.setMarkers();
+	},
 
+	setMarkers: function(){
+		initialPlaces.forEach(function(placeItem) {
+			var myLatLng = {lat: placeItem.lat, lng: placeItem.lng};
+			var marker = new google.maps.Marker({
+			    position: myLatLng,
+			    animation: google.maps.Animation.DROP,
+			    map: map,
+			    title: placeItem.name
+			});
+			//incapsulating marker Objects inside InitialPlaces
+			placeItem.marker = marker;
+		});
+	}
+
+};
 
 
 ko.applyBindings(new ViewModel());
