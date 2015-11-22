0128653a84113bfbc36eeccfe4775ca6214301fb
alessandroluciani <alessandroluciani@users.noreply.github.com>
2015-11-22 13:55:34 +0100

    some fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,25 +3,22 @@ var ViewModel = function() {
 	var rome,
         map,
         infowindow,
-        bounds;
-    var service;
+        bounds,
+        service;
    
     self.allPlaces = ko.observableArray([]);
 
     this.initialize = function() {
-
     	rome = new google.maps.LatLng(41.896046, 12.476709);
     	map = new google.maps.Map(document.getElementById('map-canvas'), {
             center: rome,
             zoom: 15,
             disableDefaultUI: true
         });
-
         this.getAllPlaces();
     };
 
     this.getAllPlaces = function(){
-
     	var request = {
             location: rome,
             radius: 1000,
@@ -33,12 +30,9 @@ var ViewModel = function() {
     };
 
     this.getAllPlacesCallback = function(results, status) {
-
     	if (status == google.maps.places.PlacesServiceStatus.OK) 
     	{
-
     		bounds = new google.maps.LatLngBounds();
-
     		results.forEach(function (place) 
     		{
     			place.marker = self.createMarker(place);
@@ -46,13 +40,11 @@ var ViewModel = function() {
                 self.allPlaces.push(place);
                 bounds.extend(new google.maps.LatLng(place.geometry.location.lat(),place.geometry.location.lng()));
     		});
-
     		map.fitBounds(bounds);
 	    }
     };
 
     this.createMarker = function(place) {
-
     	var marker = new google.maps.Marker({
             map: map,
             position: place.geometry.location,
@@ -77,33 +69,20 @@ var ViewModel = function() {
         return cityState;
     };
 
-
-
-
-    // An array that will contain all places that are initially retrieved by
-    // the getAllPlaces function.
     self.allPlaces = ko.observableArray([]);
 
-    // Array derived from allPlaces.  Contains each place that met the search
-    // criteria that the user entered.
     self.filteredPlaces = ko.computed(function () {
         return self.allPlaces().filter(function (place) {
             return place.isInFilteredList();
         });
     });
 
-    // Currently selected location.
     self.chosenPlace = ko.observable();
-
-    // Value associated with user input from search bar used to filter results.
     self.query = ko.observable('');
 
-    // Break the user's search query into separate words and make them lowercase
-    // for comparison between the places in allPlaces.
     self.searchTerms = ko.computed(function () {
         return self.query().toLowerCase().split(' ');
     });
-
 
     self.search = function () {
         self.chosenPlace(null);
@@ -114,8 +93,6 @@ var ViewModel = function() {
         });
         self.searchTerms().forEach(function (word) {
             self.allPlaces().forEach(function (place) {
-                // If search term is in the place's name or if the search term
-                // is one of the place's types, that is a match.
                 if (place.name.toLowerCase().indexOf(word) !== -1 ||
                     place.types.indexOf(word) !== -1) {
                     place.isInFilteredList(true);
@@ -125,9 +102,6 @@ var ViewModel = function() {
         });
     };
 
-
-    // Sets which place is the chosenPlace, makes its marker bounce, and
-    // displays its infowindow.
     self.selectPlace = function (place) {
         if (place === self.chosenPlace()) {
             self.displayInfo(place);
@@ -142,18 +116,8 @@ var ViewModel = function() {
         }
     };
 
-
-/////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////
-
-
-
-    // Boolean to determine whether or not to show the list view.
     self.displayingList = ko.observable(true);
 
-    // Determines which icon the button that toggles the list view will have.
-    // Based on whether or not list is currently displaying.
     self.listToggleIcon = ko.computed(function () {
         if (self.displayingList()) {
             return 'fa fa-minus-square fa-2x fa-inverse';
@@ -161,7 +125,6 @@ var ViewModel = function() {
         return 'fa fa-plus-square fa-2x fa-inverse'
     });
 
-    // If list view is shown, hide it.  Otherwise, show it.
     self.toggleListDisplay = function () {
         if (self.displayingList()) {
             self.displayingList(false);
@@ -170,18 +133,13 @@ var ViewModel = function() {
         }
     };
 
-
     self.chosenPhotoIndex = ko.observable(0);
-
-
-
 
     self.displayInfo = function (place) {
         var request = {
             placeId: place.place_id
         };
         service.getDetails(request, function (details, status) {
-            // Default values to display if getDetails fails.
             var locName = '<h4>' + place.name + '</h4>';
             var locStreet = '';
             var locCityState = '';
@@ -189,7 +147,6 @@ var ViewModel = function() {
         
             if (status == google.maps.places.PlacesServiceStatus.OK) {
                 if (details.website) {
-                    // Add a link to the location's website in the place's name.
                     locName = '<h4><a target="_blank" href=' + details.website +
                         '>' + place.name + '</a></h4>';
                 }
@@ -201,8 +158,7 @@ var ViewModel = function() {
                         details.formatted_address) + '</p>';
                     locCityState = '<p>' + self.getCityState(
                         details.formatted_address) + '<p>';
-                }
-                
+                } 
             }
             var content = '<div class="infowindow">' + locName + locStreet +
                 locCityState + locPhone + '</div>';
@@ -212,26 +168,17 @@ var ViewModel = function() {
         })
     };
 
-
-
     this.initialize();
 
-
-    // When the window is resized, update the size of the displayed photo and
-    // make sure the map displays all markers.
     window.addEventListener('resize', function (e) {
         map.fitBounds(bounds);
     });
 
-    
-    // When infowindow is closed, stop the marker's bouncing animation and
-    // deselect the place as chosenPlace.
     google.maps.event.addListener(infowindow,'closeclick',function(){
         self.chosenPlace().marker.setAnimation(null);
         self.chosenPlace(null);
     });
 
-    // When the page loads, if the width is less than 650px, hide the list view
     $(function () {
         if ($(window).width() < 650) {
             if (self.displayingList()) {
@@ -239,9 +186,6 @@ var ViewModel = function() {
             }
         }
     }());
-
 };
-
-
 
 ko.applyBindings(new ViewModel());
